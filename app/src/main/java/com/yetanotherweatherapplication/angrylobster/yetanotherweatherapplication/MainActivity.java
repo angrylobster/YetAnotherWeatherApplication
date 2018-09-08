1f129539f24e0743dab67e9ed80c80c964079b07
angrylobster <liewminshan@gmail.com>
2018-09-08 14:47:22 +0800

    Add a working ArrayAdapter and add functionality to button to add items to ArrayAdapter

--- app/src/main/java/com/yetanotherweatherapplication/angrylobster/yetanotherweatherapplication/MainActivity.java
+++ app/src/main/java/com/yetanotherweatherapplication/angrylobster/yetanotherweatherapplication/MainActivity.java
@@ -2,12 +2,56 @@ package com.yetanotherweatherapplication.angrylobster.yetanotherweatherapplicati
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> mDataSource;
+    private ListView mListView;
+    private ArrayAdapter<String> mStringArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDataSource = new ArrayList<String>();
+        mListView = (ListView) findViewById(R.id.location_list);
+
+        mDataSource.add("Singapore");
+        mDataSource.add("Malaysia");
+
+        mStringArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDataSource);
+
+        mListView.setAdapter(mStringArrayAdapter);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.button_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.add_location){
+            Toast.makeText(this, "Adding!", Toast.LENGTH_SHORT).show();
+            mDataSource.add("India");
+            mStringArrayAdapter.notifyDataSetChanged();
+        }
+
+        return true;
+    }
+
 }
